27f61dfab7ed79b459ed43167826330054541ddb
talagi <agnieszkata1@o2.pl>
2019-10-03 14:15:56 +0200

    projekt django i aplikacja pizza

--- pizza/models.py
+++ pizza/models.py
@@ -11,30 +11,21 @@ class Pizza(models.Model):
         (MEDIUM, 'Średnia'),
         (SMALL, 'Mała'),
     )
-    nazwa = models.CharField(
-        verbose_name='pizza',
-        max_length=30,
-        help_text='Nazwa pizzy')
-    opis = models.TextField(
-        blank=True,
-        help_text='Opis pizzy')
-    rozmiar = models.CharField(
-        max_length=1,
-        choices=ROZMIARY,
-        default=LARGE)
-    cena = models.DecimalField(max_digits=5, decimal_places=2)
+    nazwa = models.CharField(verbose_name='pizza', max_length=30, help_text='Nazwa pizzy')
+    opis = models.TextField(blank=True, default='', help_text='Opis pizzy')
+    rozmiar = models.CharField(max_length=1, choices=ROZMIARY, default=LARGE)
+    cena = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     data = models.DateField('dodano', auto_now_add=True)
 
-class Skladnik(models.Model):
-    pizza =  models.ManyToManyField(
-        Pizza,
-        related_name='skladniki'
-    )
-    nazwa = models.CharField('składnik', max_length=30)
-    jarski = models.BooleanField(
-        'jarski?',
-        help_text='Zaznacz, jeżeli składnik jest odpowiedni dla wegetarian',
-        default=False)
-    cena = models.DecimalField(max_digits='3', decimal_places='2')
+    def __str__(self):
+        return self.nazwa
 
+class Skladnik(models.Model):
+    pizze =  models.ManyToManyField(Pizza, related_name='skladniki')
+    nazwa = models.CharField('składnik', max_length=30)
+    jarski = models.BooleanField('jarski?', help_text='Zaznacz, jeżeli składnik jest odpowiedni dla wegetarian', default=False)
+    cena = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.nazwa
 
